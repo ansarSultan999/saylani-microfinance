07b12c7ee8c208234b04fe139684875d856ddc92
Your Name <you@example.com>
2025-01-26 08:22:58 +0500

    end

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './UserDashboard.css'; // Import the CSS file for styling
 
 interface LoanRequest {
   id: string;
